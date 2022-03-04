cccf4dc56ba4ee502295ab24cadb2fc9d3f52e86
quentin-abei <quentincrto@gmail.com>
2022-03-04 13:42:13 +0100

    newDao based of NFT sale

---
+++ hardhat/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xC150794a2C841d554a71939DA7d95459bc8ee492";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
